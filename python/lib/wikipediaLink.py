8e8eed80ec882631cf9c9db1104a5dd9a176de9e
Landon <lharri73@vols.utk.edu>
2019-12-19 16:14:15 -0600

    restructure with different branches

--- python/lib/wikipediaLink.py
+++
@@ -1,6 +0,0 @@
-class Link:
-    def __init__(self, link):
-        self._link = link
-        self.href  = "https://en.wikipedia.org" + link.attrs["href"]
-        self.title = link.attrs["title"]
-        self.name  = link.string
